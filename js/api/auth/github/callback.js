fe98f4c143c7b623831b98df58defd8b3c03fb8c
Krishnadip Choudhury <krishnadipdiyabati1977@gmail.com>
2025-11-19 10:23:54 +0530

    update

--- js/api/auth/github/callback.js
+++ js/api/auth/github/callback.js
@@ -3,9 +3,13 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
   const code = req.query.code;
   
-  const tokenResponse = await fetch(
-    `https://github.com/login/oauth/access_token`,
-    {
+  if (!code) {
+    return res.status(400).send("No code provided");
+  }
+  
+  try {
+    // Exchange code for access token
+    const tokenResponse = await fetch(`https://github.com/login/oauth/access_token`, {
       method: "POST",
       headers: { Accept: "application/json" },
       body: new URLSearchParams({
@@ -13,16 +17,25 @@ export default async function handler(req, res) {
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code: code
       })
+    });
+    
+    const tokenData = await tokenResponse.json();
+    const accessToken = tokenData.access_token;
+    
+    if (!accessToken) {
+      return res.status(400).send("Failed to get access token");
     }
-  );
-  
-  const tokenData = await tokenResponse.json();
-  const accessToken = tokenData.access_token;
-  
-  const userResponse = await fetch("https://api.github.com/user", {
-    headers: { Authorization: `Bearer ${accessToken}` }
-  });
-  
-  const userData = await userResponse.json();
-  res.send(`Hello ${userData.login}!`);
+    
+    // Get GitHub user data
+    const userResponse = await fetch("https://api.github.com/user", {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    
+    const userData = await userResponse.json();
+    
+    res.send(`<h1>Hello ${userData.login}!</h1><p>You are now logged in with GitHub.</p>`);
+    
+  } catch (error) {
+    res.status(500).send("Error during GitHub OAuth: " + error.message);
+  }
 }
