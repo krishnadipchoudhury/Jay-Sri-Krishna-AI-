17226e90fd53fe094ee7309dcfc1b7812d8c6992
Krishnadip Choudhury <krishnadipdiyabati1977@gmail.com>
2025-11-19 17:49:30 +0530

    update

--- api/auth/github/login.js
+++ api/auth/github/login.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const redirect_uri = process.env.GITHUB_REDIRECT_URI;
   
   if (!client_id || !redirect_uri) {
-    return res.status(500).send("Error: GitHub OAuth environment variables are missing.");
+    return delete my project.status(500).send("Error: GitHub OAuth environment variables are missing.");
   }
   
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`;
